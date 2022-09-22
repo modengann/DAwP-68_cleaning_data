a528770adba5485fa55000c1c735bc390fd0af98
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 10:10:15 -0400

    init

---
+++ src/cleaning_data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from numpy.core.numeric import identity
+import pandas as pd
+import numpy as np
+
+
+def cleaning_data():
+    pass
+
+def main():
+    cleaning_data()
+
+if __name__ == "__main__":
+    main()
